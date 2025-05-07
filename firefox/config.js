257b17ed7e0bf07da83ba8b7443246b5491a959f
notzetkin <86797505+notzetkin@users.noreply.github.com>
2025-05-08 02:45:02 +0300

    Add files via upload

--- firefox/config.js
+++ firefox/config.js
@@ -1,4 +1,7 @@
 // skip 1st line
+lockPref('xpinstall.signatures.required', false);
+lockPref('extensions.install_origins.enabled', false);
+lockPref("extensions.experiments.enabled", true); 
 try {
     
   let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
@@ -8,6 +11,11 @@ try {
   if(cmanifest.exists()){
     Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
     ChromeUtils.importESModule('chrome://userchromejs/content/boot.sys.mjs');
+    Services.scriptloader.loadSubScript('chrome://userchromejs/content/BootstrapLoader.js');
   }
 
 } catch(ex) {};
+
+try {
+Services.scriptloader.loadSubScript('chrome://userchromejs/content/userChrome.js'); 
+} catch (ex) {};
