ccba9b1289dbc50566859f2410e4f666b1b166d2
notzetkin <86797505+notzetkin@users.noreply.github.com>
2025-05-18 19:45:38 +0300

    back to regular loader

--- firefox/config.js
+++ firefox/config.js
@@ -1,7 +1,4 @@
 // skip 1st line
-lockPref('xpinstall.signatures.required', false);
-lockPref('extensions.install_origins.enabled', false);
-lockPref("extensions.experiments.enabled", true); 
 try {
     
   let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
@@ -11,11 +8,6 @@ try {
   if(cmanifest.exists()){
     Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
     ChromeUtils.importESModule('chrome://userchromejs/content/boot.sys.mjs');
-    Services.scriptloader.loadSubScript('chrome://userchromejs/content/BootstrapLoader.js');
   }
 
 } catch(ex) {};
-
-try {
-Services.scriptloader.loadSubScript('chrome://userchromejs/content/userChrome.js'); 
-} catch (ex) {};
