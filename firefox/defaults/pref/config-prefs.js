425de9be05246a7ea9c65685ad4ace58bb50a54b
notzetkin <86797505+notzetkin@users.noreply.github.com>
2025-08-08 23:37:19 +0300

    Add files via upload

--- firefox/defaults/pref/config-prefs.js
+++ firefox/defaults/pref/config-prefs.js
@@ -1,4 +1,5 @@
 pref("general.config.obscure_value", 0);
 pref("general.config.filename", "config.js");
+pref("userchrome.Title", "Microsoft Internet Explorer")
 // Sandbox needs to be disabled in release and Beta versions
 pref("general.config.sandbox_enabled", false);
